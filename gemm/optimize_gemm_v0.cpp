5522fc6bdbfe08341b4955656a5955998dcc7e71
LiYuRio <liyuruijx@pku.edu.cn>
2020-05-30 13:37:57 +0800

    Change the looping order

--- gemm/optimize_gemm_v0.cpp
+++ gemm/optimize_gemm_v0.cpp
@@ -9,17 +9,17 @@ void add_dot_1x4(int k, float *a, int lda, float *b, int ldb, float *c,
   int p;
   for (p = 0; p < k; p++) {
     C(0, 0) += A(0, p) * B(p, 0);
-    C(0, 1) += A(0, p) * B(p, 1);
-    C(0, 2) += A(0, p) * B(p, 2);
-    C(0, 3) += A(0, p) * B(p, 3);
+    C(1, 0) += A(1, p) * B(p, 0);
+    C(2, 0) += A(2, p) * B(p, 0);
+    C(3, 0) += A(3, p) * B(p, 0);
   }
 }
 
 void optimize_sgemm_v0(int m, int n, int k, float *a, int lda, float *b,
                        int ldb, float *c, int ldc) {
   int i, j, p;
-  for (i = 0; i < m; i++) {
-    for (j = 0; j < n; j+=4) {
+  for (i = 0; i < m; i += 4) {
+    for (j = 0; j < n; j++) {
       add_dot_1x4(k, &A(i, 0), lda, &B(0, j), ldb, &C(i, j), ldc);
     }
   }
